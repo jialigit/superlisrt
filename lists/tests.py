a25b3680ca1f97fe38965ae0c911a0912bb9546b
ice <ice@bogon.dhcp>
2017-09-02 22:44:36 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -12,4 +14,12 @@ class HomePafeTest(TestCase):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
+    def test_home_page(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
         
