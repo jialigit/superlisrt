e1ed4a0719d4e981e7eba86c92b225f2a47073a0
ice <ice@bogon.dhcp>
2017-09-03 11:18:30 +0800

    Front page HTML now generated from a tempalte

--- functional_test.py
+++ functional_test.py
@@ -35,7 +35,8 @@ class NewVistorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
 
         self.assertTrue(
-            any(row.text == '1: Buy peacoak feathers' for row in rows)
+            any(row.text == '1: Buy peacoak feathers' for row in rows),
+            'new to-do item did not appear in table'
             )
 
 
